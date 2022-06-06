92bb15723115c7e0a7ed10c8698c52314f27ed7f
Mark Kennedy <kennedymark@hotmail.com>
2022-06-06 12:01:20 -0400

    created Game component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Game from './components/Game'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Game />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
