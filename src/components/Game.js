5f4c8d45d69533fece76054160dc848a78eb03ca
Mark Kennedy <kennedymark@hotmail.com>
2022-06-06 16:21:08 -0400

    created checkHaveValue and checkEqual functions

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 
 const Game = () => {
+  const [xAndO, setXAndO] = useState('X')
   const [board, setBoard] = useState([
     null,
     null,
@@ -16,15 +17,33 @@ const Game = () => {
 
   const squareClick = (index) => {
     let newBoard = board
-    newBoard[index] = 'X'
+    newBoard[index] = xAndO
     setBoard(newBoard)
+    xAndO === 'X' ? setXAndO('O') : setXAndO('X')
   }
+
+  const checkHaveValue = (b) => {
+    if (b[0] && b[1] && b[2]) checkEqual(b[0], b[1], b[2])
+    if (b[3] && b[4] && b[5]) checkEqual(b[3], b[4], b[5])
+    if (b[6] && b[7] && b[8]) checkEqual(b[6], b[7], b[8])
+    if (b[0] && b[3] && b[6]) checkEqual(b[0], b[2], b[6])
+    if (b[1] && b[4] && b[7]) checkEqual(b[1], b[4], b[7])
+    if (b[2] && b[5] && b[8]) checkEqual(b[2], b[5], b[8])
+    if (b[0] && b[4] && b[8]) checkEqual(b[0], b[4], b[8])
+    if (b[2] && b[4] && b[6]) checkEqual(b[1], b[4], b[6])
+  }
+
+  const checkEqual = (a, b, c) => {
+    if (a === b && a === c) console.log(`${a} has Won!`)
+  }
+
+  checkHaveValue(board)
 
   return (
     <div className="board">
       {board.map((s, index) => (
         <div className="square" key={index} onClick={() => squareClick(index)}>
-          {board[index]}
+          {s}
         </div>
       ))}
     </div>
