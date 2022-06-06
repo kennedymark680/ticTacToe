1c64964dfe89ceb25d058dc855dfe272120ff678
Mark Kennedy <kennedymark@hotmail.com>
2022-06-06 13:18:17 -0400

    created board

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Game = () => {
-  return <div>Game</div>
+  const [board, setBoard] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null
+  ])
+
+  return (
+    <div className="board">
+      {board.map((s) => (
+        <div className="square"></div>
+      ))}
+    </div>
+  )
 }
 
 export default Game
